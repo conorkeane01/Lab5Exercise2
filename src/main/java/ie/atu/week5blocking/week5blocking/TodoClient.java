1243bf3450f5d302ec9d2eddb33eef0c9df5038a
conorkeane <g00391846@atu.ie>
2023-10-22 16:31:45 +0100

    Added TodoResponse

--- src/main/java/ie/atu/week5blocking/week5blocking/TodoClient.java
+++ src/main/java/ie/atu/week5blocking/week5blocking/TodoClient.java
@@ -1,4 +1,10 @@
 package ie.atu.week5blocking.week5blocking;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "jsonplaceholder", url = "https://jsonplaceholder.typicode.com")
 public interface TodoClient {
+    @GetMapping("/todos/1")
+    TodoResponse fetchData();
 }
