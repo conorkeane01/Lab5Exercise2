3e242497ee21b83cce63b6c43a21173cf96dc532
conorkeane <g00391846@atu.ie>
2023-10-22 17:10:12 +0100

    Added code to feignService class

--- src/main/java/ie/atu/week5blocking/week5blocking/FeignService.java
+++ src/main/java/ie/atu/week5blocking/week5blocking/FeignService.java
@@ -1,4 +1,23 @@
 package ie.atu.week5blocking.week5blocking;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeignService {
+    private final TodoClient todoClient;
+
+    public FeignService(TodoClient todoClient) {
+        this.todoClient = todoClient;
+    }
+
+    public TodoResponse fetchData() {
+        TodoResponse td = todoClient.fetchData();
+        System.out.println(td);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return td;
+    }
 }
