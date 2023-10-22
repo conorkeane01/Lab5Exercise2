ccfccde1b3241379b016ac019b6f76bb884a9b20
conorkeane <g00391846@atu.ie>
2023-10-22 14:17:48 +0100

    Created new project

---
+++ src/test/java/ie/atu/week5blocking/week5blocking/Week5BlockingApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5blocking.week5blocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5BlockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
